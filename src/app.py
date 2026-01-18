64e358fa5815881c32b0fc2db708383ef1337fdc
Suds Kumar <suds123@gmail.com>
2026-01-18 09:34:40 +0000

    updated app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/api/v1/details')
 def details():
-    return 'Hello World!'
+    return jsonify({
+        'name': 'John Doe',
+        'age': 30,
+        'city': 'New York'
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
