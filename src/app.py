c72134bb3c6089ff544bd389c9f7faa9489ae39b
Suds Kumar <suds123@gmail.com>
2026-01-18 09:37:25 +0000

    updated

--- src/app.py
+++ src/app.py
@@ -10,5 +10,11 @@ def details():
         'city': 'New York'
     })
 
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({
+        'status': 'ok'
+    }), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
