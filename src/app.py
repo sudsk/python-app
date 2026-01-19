03d85712e2ab4cfbd518d224014f3a123897dd8e
Suds Kumar <suds123@gmail.com>
2026-01-19 17:18:52 +0000

    testing

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
         'hostname': socket.gethostname(),
         'ip': socket.gethostbyname(socket.gethostname()),
         'timestamp': datetime.datetime.now().isoformat(),
-        'message': 'Test of New CI/CD pipeline!',
+        'message': 'Testing New CI/CD pipeline!',
     })
 
 @app.route('/api/v1/healthz')
